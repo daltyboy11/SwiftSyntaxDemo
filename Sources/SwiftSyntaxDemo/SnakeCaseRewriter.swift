6b4688f6455b0447c8c44055cfb70ba4501ea0ee
Dalton Sweeney <daltyboy11@gmail.com>
2019-11-14 00:29:36 -0800

    update SnakeCaseRewriter to handle rewriting function parameters

--- Sources/SwiftSyntaxDemo/SnakeCaseRewriter.swift
+++ Sources/SwiftSyntaxDemo/SnakeCaseRewriter.swift
@@ -12,14 +12,24 @@ final class SnakeCaseRewriter: SyntaxRewriter {
   }
 
   /// Removes all underscores from the identifier and capitalizes characters
-  /// following underscores. Assumes `identifier` is a valid identifier.
+  /// following underscores. Assumes `identifier` is a valid identifier. Ignores
+  /// leading underscores.
   private func convertToCamelCase(_ identifier: String) -> String {
     let identifier = Array(identifier)
     var newIdentifier = ""
     var i = 0
+    var hasSeenNonUnderscoreCharacter = false
     while i < identifier.count {
-      if identifier[i] == "_" && i+1 < identifier.count &&
-      ("a"..."z").contains(identifier[i+1]) {
+      if identifier[i] != "_" {
+        hasSeenNonUnderscoreCharacter = true
+      }
+
+      if identifier[i] == "_" && !hasSeenNonUnderscoreCharacter {
+        newIdentifier.append("_")
+        i += 1
+      } else if identifier[i] == "_" &&
+          i+1 < identifier.count &&
+          ("a"..."z").contains(identifier[i+1]) {
         newIdentifier.append(identifier[i+1].uppercased())
         i += 2
       } else if identifier[i] != "_" {
@@ -52,6 +62,32 @@ final class SnakeCaseRewriter: SyntaxRewriter {
       let newNode = node.withIdentifier(newToken)
       return super.visit(newNode)
     }
+    return super.visit(node)
+  }
+
+  override func visit(_ node: FunctionParameterSyntax) -> Syntax {
+    // If both firstName and secondName are non nil then it's a function
+    // parameter name like foo(withX x: ...) and we want to modify the
+    // secondName. If only firstName is non nil then it's a function parameter
+    // like foo(x: ...) and we wont to modify the first name.
+    if let firstNameToken = node.firstName,
+      let secondNameToken = node.secondName,
+      case .identifier(let identifier) = secondNameToken.tokenKind,
+      isSnakeCase(identifier) {
+      let newIdentifier = convertToCamelCase(identifier)
+      let newSecondName = secondNameToken.withKind(.identifier(newIdentifier))
+      let newNode = node.withSecondName(newSecondName)
+      return super.visit(newNode)
+    } else if let firstNameToken = node.firstName,
+      case .identifier(let identifier) = firstNameToken.tokenKind,
+      isSnakeCase(identifier) {
+      let newIdentifier = convertToCamelCase(identifier)
+      let newFirstName = firstNameToken.withKind(.identifier(newIdentifier))
+      let newNode = node.withFirstName(newFirstName)
+      return super.visit(newNode)
+    }
+    
+    // We should never get here. You can't have an unnamed parameter!
     return super.visit(node)
   }
 }
--- Tests/SwiftSyntaxDemoTests/SwiftSyntaxDemoTests.swift
+++ Tests/SwiftSyntaxDemoTests/SwiftSyntaxDemoTests.swift
@@ -6,29 +6,41 @@ import SwiftSyntax
 final class SwiftSyntaxDemoTests: XCTestCase {
   private let testDirectoryPrefix = "./Tests/SwiftSyntaxDemoTests/"
 
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct
-        // results.
-        XCTAssertEqual(SwiftSyntaxDemo().text, "Hello, World!")
-    }
+  func testExample() {
+      // This is an example of a functional test case.
+      // Use XCTAssert and related functions to verify your tests produce the correct
+      // results.
+      XCTAssertEqual(SwiftSyntaxDemo().text, "Hello, World!")
+  }
 
-    func testIntegerLiteralFormatter() {
-      let url = URL(fileURLWithPath: testDirectoryPrefix + "input1.txt")
-      let sourceFile = try! SyntaxTreeParser.parse(url)
-      let reformattedSource = IntegerLiteralFormatter().visit(sourceFile)
-      XCTAssertEqual(
-        IntegerLiteralFormatterChecker().hasNonUnderscoreSeparatedIntegerLiterals(reformattedSource),
-        false)
-    }
+  func testIntegerLiteralFormatter() {
+    let url = URL(fileURLWithPath: testDirectoryPrefix + "input1.txt")
+    let sourceFile = try! SyntaxTreeParser.parse(url)
+    let reformattedSource = IntegerLiteralFormatter().visit(sourceFile)
+    XCTAssertEqual(
+      String(describing: reformattedSource),
+      try! String(contentsOfFile: testDirectoryPrefix + "expected1.txt", encoding: .utf8))
+  }
 
-    func testSnakeCaseRewriter() {
-      let url = URL(fileURLWithPath: testDirectoryPrefix + "input2.txt")
-      let sourceFile = try! SyntaxTreeParser.parse(url)
-      let reformattedSource = SnakeCaseRewriter().visit(sourceFile)
-    }
+  func testSnakeCaseRewriter1() {
+    let url = URL(fileURLWithPath: testDirectoryPrefix + "input2.txt")
+    let sourceFile = try! SyntaxTreeParser.parse(url)
+    let reformattedSource = SnakeCaseRewriter().visit(sourceFile)
+    XCTAssertEqual(
+      String(describing: reformattedSource),
+      try! String(contentsOfFile: testDirectoryPrefix + "expected2.txt", encoding: .utf8))
+  }
 
-    static var allTests = [
-        ("testExample", testExample),
-    ]
+  func testSnakeCaseRewriter2() {
+    let url = URL(fileURLWithPath: testDirectoryPrefix + "input3.txt")
+    let sourceFile = try! SyntaxTreeParser.parse(url)
+    let reformattedSource = SnakeCaseRewriter().visit(sourceFile)
+    XCTAssertEqual(
+      String(describing: reformattedSource),
+      try! String(contentsOfFile: testDirectoryPrefix + "expected3.txt", encoding: .utf8))
+  }
+
+  static var allTests = [
+      ("testExample", testExample),
+  ]
 }
